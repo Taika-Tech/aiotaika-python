192fb53f978218aeebf0fa3cbe4099863defda20
Jussi <jussi.hietanen@iki.fi>
2023-04-25 16:08:33 +0300

    Add copyright notice to source files

--- aiotaika/const.py
+++ aiotaika/const.py
@@ -1,4 +1,13 @@
+# Taika asynchronous client library for Python
+#    Copyright (C) 2023 Taika Tech Oy
+#    Author Jussi Hietanen
+#
+# This library is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free Software
+# Foundation. See the GNU Lesser General Public License for more details.
+
 """Constants."""
+
 from typing import Final
 
 DEFAULT_DB_PORT: Final = 3306
--- aiotaika/locator.py
+++ aiotaika/locator.py
@@ -1,3 +1,11 @@
+# Taika asynchronous client library for Python
+#    Copyright (C) 2023 Taika Tech Oy
+#    Author Jussi Hietanen
+#
+# This library is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free Software
+# Foundation. See the GNU Lesser General Public License for more details.
+
 from collections.abc import Awaitable, Callable
 from dataclasses import dataclass
 from typing import Any
